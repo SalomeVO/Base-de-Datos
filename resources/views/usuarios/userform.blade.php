710d317d5e07a958aac79f7a6eff9d05164ba4d7
SalomeVO <mvasquezo10@miumg.edu.gt>
2021-10-29 10:43:22 -0600

    se mejoro la interfaz del formulario de agregar usuario

--- resources/views/usuarios/userform.blade.php
+++ resources/views/usuarios/userform.blade.php
@@ -1,78 +1,81 @@
 @extends('layouts.base')
+@section('title', 'Formulario')
 @section('content')
 
-<div class="container mt-5">
 
-    <div class="row justify-content-center">
-        <div class="col-md-7 mt-5">
-        <!--Mensaje flash-->
-        @if(session("usuarioGuardado"))
-        <div class="alert alert-success">
-            {{session("usuarioGuardado")}}
-        </div>
-        @endif
+    <div class="container mt-5">
+
+        <div class="row justify-content-center">
+            <div class="col-md-7 mt-5">
+                <!--Mensaje flash-->
+                @if(session("usuarioGuardado"))
+                    <div class="alert alert-success text-dark">
+                        {{session("usuarioGuardado")}}
+                    </div>
+                @endif
 
             <!--Validacion de errores-->
-        @if($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{$error}}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
-            <div class="card">
-                <form action="{{ url ('/save') }}" method="POST" enctype="multipart/form-data"><!--para que acepte imagenes-->
-                @csrf
+                <div class="card border-success mb-3">
+                    <form action="{{ url ('/save') }}" method="POST" enctype="multipart/form-data"><!--para que acepte imagenes-->
+                        @csrf
 
-                    <div class="card-header text-center">AGREGAR USUARIO</div>
+                        <div class="card-header text-center text-white bg-success">AGREGAR USUARIO</div>
 
-                    <div class="card-body">
+                        <div class="card-body" style="background-color: #F8F8FF;" >
 
-                        <div class="row form-group">
-                            <label for="" class="col-2">Nombre</label>
-                            <input type="text" name="nombre" class="form-control col-md-9">
-                        </div>
+                            <div class="row form-group">
+                                <label for="" class="col-2">Nombre</label>
+                                <input type="text" name="nombre" class="form-control col-md-9" placeholder="nombre">
+                            </div>
 
-                        <div class="row form-group">
-                            <label for="" class="col-2">Email</label>
-                            <input type="text" name="email" class="form-control col-md-9">
-                        </div>
+                            <div class="row form-group">
+                                <label for="" class="col-2">Email</label>
+                                <input type="text" name="email" class="form-control col-md-9" placeholder="name@gmail.com">
+                            </div>
 
-                        <!--para el formulario de imagenes-->
-                        <div class="row form-group">
-                            <label for="" class="col-2" >Imagen</label>
-                            <input type="file" name="imagenes" class="img-fluid">
-                        </div>
+                            <!--para el formulario de imagenes-->
+                            <div class="row form-group">
+                                <label for="" class="col-2" >Imagen</label>
+                                <input type="file" name="imagenes" class="img-fluid">
+                            </div>
 
-                        <!--rol vista-->
-                        <div class="row mb-3">
-                            <div class="col-6 offset-3">
-                                <div class="form-group">
-                                    <label>Rol</label>
-                                    <select name="rol_id" class="form-control" >
-                                        <option value="">--Seleccione--</option>
+                            <!--rol vista-->
+                            <div class="row mb-3">
+                                <div class="col-6 offset-3">
+                                    <div class="form-group">
+                                        <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Rol</label>
+                                        <select name="rol_id" class="custom-select mr-sm-2" id="inlineFormCustomSelect" >
+                                            <option class="align-self-center text-center" value="">--Rol--</option>
 
-                                        @foreach( $rol as $roles)
-                                            <option value="{{$roles->id_rol}}"> {{$roles->descripcion}}  </option>
-                                        @endforeach
-                                    </select>
+                                            @foreach( $rol as $roles)
+                                                <option value="{{$roles->id_rol}}"> {{$roles->descripcion}}  </option>
+                                            @endforeach
+
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
+
+                            <div class="row form-group">
+                                <button type="submit" class="btn btn-success col-md-9 offset-2 text-dark" style="background-color: #3CB371;">Guardar</button>
+                            </div>
+
                         </div>
 
-                        <div class="row form-group">
-                            <button type="submit" class="btn btn-success col-md-9 offset-2">Guardar</button>
-                        </div>
+                    </form>
+                </div>
 
-                    </div>
-
-                </form>
             </div>
-
         </div>
     </div>
-</div>
 @endsection
